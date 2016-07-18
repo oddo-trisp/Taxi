8843aaac1f2b03895825730c0e5380539fe1681d
Odysseas Trispiotis <odysseastrispiotis@MacBook-Pro.local>
2016-07-18 20:27:11 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 	<meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis">
+ <body>
+ <?php
+    session_start();
+?>
+ <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<input type="submit" name="en" value="English" />
+	<input type="submit" name="gr" value="Ελληνικά" />
+ </form>
+	<?php
+ 		if (isset($_GET['en'])){
+        	//echo ('Hello World');
+        	$_SESSION['language']='en';
+    	}
+    	else if(isset($_GET['gr'])){
+    		$_SESSION['language']='gr';
+        	//echo ('Γεια σου κόσμε');
+    	}
+    	if ($_SESSION['language']=='en'){
+        	echo ('Hello World');
+        	//$_SESSION['language']='en';
+    	}
+    	else {
+    		//$_SESSION['language']='gr';
+        	echo ('Γεια σου κόσμε');
+    	}
+	?> 
+ <a href="nums.php">Numbers</a>
+ </body>
+</html>
---
+++ nums.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 	<meta name="author" content="Giorgos Apostolopoulos, Odysseas Trispiotis">
+ <body>
+ <?php
+    session_start();
+?>
+ <form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	<input type="submit" name="en" value="English" />
+	<input type="submit" name="gr" value="Ελληνικά" />
+ </form>
+	<?php
+
+ 		if (isset($_GET['en'])){
+            //echo ('Hello World');
+            $_SESSION['language']='en';
+        }
+        else if(isset($_GET['gr'])){
+            $_SESSION['language']='gr';
+            //echo ('Γεια σου κόσμε');
+        }
+        if ($_SESSION['language'] == 'en'){
+            echo ('one two three');
+            //$_SESSION['language']='en';
+        }
+        else {
+            //$_SESSION['language']='gr';
+            echo ('ένα δύο τρία');
+        }
+	?> 
+ 
+ </body>
+</html>
