06643244248d99313b698ec6b36a7d110602d904
oddo-trisp <oddo1994.ot@gmail.com>
2016-07-19 20:11:10 +0300

    images

--- index.php
+++ index.php
@@ -129,12 +129,12 @@
                 <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <img class="img-circle img-responsive img-center" src="assets/img/delfi.jpg" alt="">
                 <h3>Marketing Box #2</h3>
                 <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
             </div>
             <div class="col-sm-4">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/300x300" alt="">
+                <img class="img-circle img-responsive img-center" src="assets/img/galaxidi.jpg" alt="">
                 <h3>Marketing Box #3</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
             </div>
