a1fbabca38ba7ebf2eae1c0fbb23f90deec93c92
oddo-trisp <oddo1994.ot@gmail.com>
2016-08-05 19:30:40 +0300

    metadata

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="24/7 taxi driver from Itea, Fokida. Transportation all around the country, as well as package transportation and package delivery . Trip organizations, in Archeological sites, Museums, Athena's temple in Delphi and wherever you desire. Transportation towards and from the airport at low cost!!">
+    <meta name="author" content="George Apostolopoulos, Odysseas Trispiotis">
 
-    <title>Georgios Trismpiotis-Taxi</title>
+    <title>Taxi-Delphi</title>
 
     <link href='https://fonts.googleapis.com/css?family=Ubuntu+Condensed&subset=greek,latin' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Noto+Serif&subset=greek,latin' rel='stylesheet' type='text/css'>
