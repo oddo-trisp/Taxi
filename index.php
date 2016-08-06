2a874f2282df32e982c140c80a524c823a7521ad
oddo-trisp <oddo1994.ot@gmail.com>
2016-08-06 15:58:56 +0300

    image buttons

--- index.php
+++ index.php
@@ -63,6 +63,10 @@
                         echo ("<a class='navbar-brand' href='#'>Υπηρεσίες Ταξί</a>");
                     }
                 ?>
+                 <form class="pull-right" method="GET" id="lang" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <input type="image" src="assets/img/grflag.png" name="gr" value="submit" alt="submit" />
+                    <input type="image" src="assets/img/ukflag.png" name="en" value="submit" alt="submit" />
+                </form>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
 
@@ -103,12 +107,7 @@
                 ?>
 
                 </ul>
-                <form class="pull-right" method="GET" id="lang" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                    <input type="image" src="assets/img/grflag.png" name="gr" value="submit" alt="submit" />
-                    <!--<input type="submit" name="gr" class="gr" />-->
-                    <!--<input type="submit" name="en" class="en" />-->
-                    <input type="image" src="assets/img/ukflag.png" name="en" value="submit" alt="submit" />
-                </form>
+               
                 <!--<form method="GET" id="lang">-->
             </div>
             <!-- /.navbar-collapse -->
